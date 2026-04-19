77fb8f68351e08b61c964beff596ba85201d1bb4
Somtochukwu Emmanuel <somtoume1@gmail.com>
2026-04-19 05:26:18 +0100

    fix duplicate User declaration

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,18 +1,24 @@
+
 const express = require('express')
 const router = express.Router()
-const {getTickets, createTicket, getTicket, deleteTicket, updateTicket} = require('../controllers/ticketController')
 
-const {protect} = require('../middleware/authMiddleware')
+const {
+  getTickets,
+  createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket
+} = require('../controllers/ticketController')
 
-// Re-route into noteRouter
-const noteRouter = require('./noteRoutes')
-router.use('/:ticketId/notes', noteRouter)
+const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(protect, getTickets).post(protect, createTicket)
-router
-.route('/:id')
-.get(protect, getTicket)
-.delete(protect, deleteTicket)
-.put(protect, updateTicket)
+router.route('/')
+  .get(protect, getTickets)
+  .post(protect, createTicket)
+
+router.route('/:id')
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket)
 
 module.exports = router
